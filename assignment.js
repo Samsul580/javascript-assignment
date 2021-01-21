247588b031e80389b65b89ee333c25242386b8ac
Samsul580 <samsulshekh2019@gmai.com>
2021-01-22 01:28:29 +0600

    kilometer to meter problem solving completed

---
+++ assignment.js
@@ -0,0 +1,18 @@
+
+
+// kilometer to meter problem solving start
+function kilometerToMeter(kilometer) {
+    if (kilometer > 0) {
+        var meter = kilometer * 1000;
+        return meter;
+    }
+    else {
+        return "distance cannot be negative or 0";
+    }
+}
+console.log(kilometerToMeter(10));
+// kilometer to meter problem solving end
+
+// hotelCost to meter problem solving start
+
+// hotelCost to meter problem solving end
